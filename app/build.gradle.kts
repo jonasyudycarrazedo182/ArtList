4921fa2380cb91647338dcd6419e84c791928fd1
Jonas <jonasyudycarrazedo182@gmail.com>
2025-02-04 20:45:06 -0300

    create artist list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+
+    implementation ("com.squareup.picasso:picasso:2.8")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
